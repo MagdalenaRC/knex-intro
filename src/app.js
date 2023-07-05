d72a1e11948494ec84e610fc59912b0969bcb5e8
MagdalenaRC <magdalena.r.carr@gmail.com>
2023-07-05 11:02:45 -0400

    knex walkthrough

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('expresss')
+const app = express();
+const PORT = process.env.PORT || 3000;
+const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV||'development']);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Application up and running.')
+})
+
+app.get('/pets', function(req, res) {
+  knex('pet')
+    .select('*')
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on ${PORT}`);
+});
+
